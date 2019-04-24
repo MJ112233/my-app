ea781d9be9ab547f2c40e25140f30eb234ab4e51
mac <sumeimeisu@icloud.com>
2019-04-24 17:02:01 +0800

    初始化
    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import section1img from './images/section1img.png';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div className="download">
+              <img className='bgimage' src={section1img}/>
+              <div className="btn-grp">
+                <a href="http://tiantianzhongappxiazai.com/ttzcpys.apk" target="_blank" className="btn-down-android">
+                      <i className="icon i-android"></i>
+                      <text className="a-txt i-act">Android版下载</text>
+                  </a>
+                  <a href="https://itunes.apple.com/cn/app/id1439440743?mt=8" target="_blank"
+                     className="btn-down-iphone mt20">
+                      <i className="icon i-iphone"></i>
+                      <text className="a-txt">iPhone版下载</text>
+                  </a>
+                  <a href="https://vnsr1007.com" target="_blank" className="pc">
+                      <text>点击进入官方网站</text>
+                  </a>
+              </div>
+              {/*<span className="qr-code"><img src="./天天中平台_files/ewm.png" width="166" height="166"></span>*/}
+          </div>
+
       </header>
     </div>
   );
